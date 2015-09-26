9e2c4f56bfb7eb1d24f2570e83b971b0b3003396
Kevin Yokley <kyokley2@gmail.com>
2015-09-26 12:26:55 -0500

    Removing thread setting

--- convert.py
+++ convert.py
@@ -52,7 +52,6 @@ def encode(source, dest, dryRun=False):
                         "libmp3lame",
                         ])
 
-    command.append('-threads 4')
     command.append(dest)
     command = tuple(command)
 
