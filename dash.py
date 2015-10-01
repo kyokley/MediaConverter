831cffed2689161dff161886606339534a103259
Kevin Yokley <kyokley2@gmail.com>
2015-10-01 18:18:59 -0500

    Adding code for dash button

---
+++ dash.py
@@ -0,0 +1,16 @@
+from scapy.all import sniff, ARP
+from main import main
+
+GATORADE = '74:75:48:41:3c:1c'
+
+def arp_capture(pkt):
+    if pkt[ARP].op == 1: #who-has (request)
+        if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+            if pkt[ARP].hwsrc == GATORADE:
+                main()
+
+def dash():
+    sniff(prn=arp_capture, filter="arp", store=0)
+
+if __name__ == '__main__':
+    dash()
