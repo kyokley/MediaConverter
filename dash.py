8b2e7ac5691a0f25b88b5c0f99421b26df4410c0
Kevin Yokley <kyokley2@gmail.com>
2015-10-03 11:36:43 -0500

    Adding sleep interval

--- dash.py
+++ dash.py
@@ -1,8 +1,10 @@
 from scapy.all import sniff, ARP
 from main import main
 from datetime import datetime
+import time
 
 GATORADE = '74:75:48:41:3c:1c'
+SLEEP_INTERVAL = 60 * 2
 
 def arp_capture(pkt):
     if pkt[ARP].op == 1: #who-has (request)
@@ -10,6 +12,10 @@ def arp_capture(pkt):
             if pkt[ARP].hwsrc == GATORADE:
                 print '%s: Got a button press! Run the converter!' % datetime.now()
                 main.delay()
+
+                # Amazon Dash tries to send a couple of messages
+                # so sleep for a bit to ignore them.
+                time.sleep(SLEEP_INTERVAL)
 
 def dash():
     sniff(prn=arp_capture, filter="arp", store=0)
