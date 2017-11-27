0504b6d7eb05d388abd5f8053b21d2119ad263c8
Kevin Yokley <kyokley2@gmail.com>
2017-11-27 16:36:30 -0600

    Adding tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,11 @@
 import unittest
 import mock
 
-from utils import stripUnicode
+from utils import (stripUnicode,
+                   is_valid_media_file,
+                   is_valid_subtitle_file,
+                   file_ext,
+                   )
 
 class TestStripUnicode(unittest.TestCase):
     def setUp(self):
@@ -70,3 +74,99 @@ class TestStripUnicode(unittest.TestCase):
         self.mock_getcwd.assert_called_once_with()
         self.mock_rename.assert_called_once_with('test_filename',
                                                  'test_stripped_filename')
+
+class TestIsValidMediaFile(unittest.TestCase):
+    def setUp(self):
+        self.exists_patcher = mock.patch('utils.os.path.exists')
+        self.mock_exists = self.exists_patcher.start()
+
+        self.MEDIA_FILE_EXTENSIONS_patcher = mock.patch('utils.MEDIA_FILE_EXTENSIONS', ('.mp4',))
+        self.MEDIA_FILE_EXTENSIONS_patcher.start()
+
+        self.mock_exists.return_value = True
+
+    def tearDown(self):
+        self.exists_patcher.stop()
+        self.MEDIA_FILE_EXTENSIONS_patcher.stop()
+
+    def test_file_does_not_exist(self):
+        self.mock_exists.return_value = False
+
+        expected = False
+        actual = is_valid_media_file('test_path.mp4')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.mp4')
+
+    def test_bad_extension(self):
+        expected = False
+        actual = is_valid_media_file('test_path.txt')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.txt')
+
+    def test_valid(self):
+        expected = True
+        actual = is_valid_media_file('test_path.mp4')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.mp4')
+
+    def test_ignored_case(self):
+        expected = True
+        actual = is_valid_media_file('test_path.MP4')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.MP4')
+
+
+class TestIsValidSubtitleFile(unittest.TestCase):
+    def setUp(self):
+        self.exists_patcher = mock.patch('utils.os.path.exists')
+        self.mock_exists = self.exists_patcher.start()
+
+        self.SUBTITLE_EXTENSIONS_patcher = mock.patch('utils.SUBTITLE_EXTENSIONS', ('.srt',))
+        self.SUBTITLE_EXTENSIONS_patcher.start()
+
+        self.mock_exists.return_value = True
+
+    def tearDown(self):
+        self.exists_patcher.stop()
+        self.SUBTITLE_EXTENSIONS_patcher.stop()
+
+    def test_file_does_not_exist(self):
+        self.mock_exists.return_value = False
+
+        expected = False
+        actual = is_valid_subtitle_file('test_path.srt')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.srt')
+
+    def test_bad_extension(self):
+        expected = False
+        actual = is_valid_subtitle_file('test_path.txt')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.txt')
+
+    def test_valid(self):
+        expected = True
+        actual = is_valid_subtitle_file('test_path.srt')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.srt')
+
+    def test_ignored_case(self):
+        expected = True
+        actual = is_valid_subtitle_file('test_path.SRT')
+
+        self.assertEqual(expected, actual)
+        self.mock_exists.assert_called_once_with('test_path.SRT')
+
+class TestFileExt(unittest.TestCase):
+    def test_file_ext(self):
+        expected = '.mp4'
+        actual = file_ext('test_path.mp4')
+
+        self.assertEqual(expected, actual)
