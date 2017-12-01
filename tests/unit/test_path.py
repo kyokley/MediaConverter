ad2ee6652857301c67ee9c22bce13825edfdd065
Kevin Yokley <kyokley2@gmail.com>
2017-12-01 11:22:40 -0600

    Separating functional tests

--- tests/unit/test_path.py
+++ tests/unit/test_path.py
@@ -1,8 +1,5 @@
 import unittest
 import mock
-import tempfile
-import shutil
-import os
 
 from path import Path
 
@@ -444,24 +441,3 @@ class TestGetMovieAndTVPathByLocalPathAndRemotePath(unittest.TestCase):
         self.mock_getPathByLocalPathAndRemotePath.assert_called_once_with('test_localpath',
                                                                           'test_remotepath',
                                                                           useMovieURL=True)
-
-class TestGetLocalPathsFunctional(unittest.TestCase):
-    def setUp(self):
-        self.temp_dir = tempfile.mkdtemp()
-        self.path = Path('localpath', 'remotepath')
-
-    def tearDown(self):
-        shutil.rmtree(self.temp_dir)
-
-    def test_path_does_not_exist(self):
-        filepath = os.path.join(self.temp_dir, 'test_file')
-        expected = set()
-        actual = self.path._buildLocalPaths([filepath])
-        self.assertEqual(expected, actual)
-
-    def test_paths_exist(self):
-        files = set([tempfile.mkstemp(dir=self.temp_dir)[1]
-                    for i in xrange(3)])
-        expected = files
-        actual = self.path._buildLocalPaths([self.temp_dir])
-        self.assertEqual(expected, actual)
