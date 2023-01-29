68684241d6038bef2f80234ba2e4e2ba8c206445
Kevin Yokley <kyokley2@gmail.com>
2023-01-29 17:39:48 -0600

    Handle bytes in utility function

--- utils.py
+++ utils.py
@@ -89,6 +89,8 @@ def send_email(subject, body):
 
 
 def is_valid_media_file(path):
+    if isinstance(path, bytes):
+        path = path.decode('utf-8')
     path = Path(path)
     return path.suffix.lower() in MEDIA_FILE_EXTENSIONS
 
