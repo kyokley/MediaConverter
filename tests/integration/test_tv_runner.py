714c36ebc813c6f11e15b51c0c9dd833ea546d69
Kevin Yokley <kyokley2@gmail.com>
2024-01-31 21:08:47 -0600

    Initial integration test

--- tests/integration/test_tv_runner.py
+++ tests/integration/test_tv_runner.py
@@ -1,10 +1,26 @@
 import pytest
+from pathlib import Path
+from tv_runner import TvRunner
 
 
 class TestTvRunner:
     @pytest.fixture(autouse=True)
-    def setUp(self):
-        pass
+    def setUp(self, mocker, tmpdir):
+        mocker.patch('tv_runner.LOCAL_TV_SHOWS_PATHS',
+                     [str(tmpdir)])
+        mocker.patch('tv_runner.DOMAIN',
+                     'http://mediaviewer:8000')
+        mocker.patch("tv_runner.MINIMUM_FILE_SIZE", 100)
 
-    def test_run(self):
+        self.temp_dir = Path(tmpdir)
+
+        self.tv_dir = self.temp_dir / "Test.Dir.Path"
+        self.tv_dir.mkdir()
+
+    def test_run(self, create_video_file):
+        dst = create_video_file(self.tv_dir,
+                                'mov_bbb.mp4')
+
+        tv_runner = TvRunner()
+        tv_runner.load_paths()
         pass
